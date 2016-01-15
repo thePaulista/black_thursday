8a395647bf88250de6c7df3fb55867a09e07d35f
Matt Pindell <pindell.m@gmail.com>
2016-01-15 08:22:38 -0700

    added .to_i onto @customer_id and @merchant_id ivars

--- lib/invoice.rb
+++ lib/invoice.rb
@@ -4,11 +4,13 @@ class Invoice
 
   def initialize(args_hash)
     @id          = args_hash[:id].to_i
-    @customer_id = args_hash[:customer_id]
-    @merchant_id = args_hash[:merchant_id]
+    @customer_id = args_hash[:customer_id].to_i
+    @merchant_id = args_hash[:merchant_id].to_i
     @status      = args_hash[:status]
-    @created_at  = args_hash[:created_at]
-    @updated_at  = args_hash[:updated_at]
+    @created_at  = Time.parse(args_hash[:created_at])
+    @updated_at  = Time.parse(args_hash[:updated_at])
+    # @created_at  = args_hash[:created_at]
+    # @updated_at  = args_hash[:updated_at]
   end
 
 end

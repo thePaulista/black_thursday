4882f7a3f5adaef963e658e4141af8536921ed0e
Matt Pindell <pindell.m@gmail.com>
2016-01-15 16:51:37 -0700

    updated to Hedy's version of Sales Analyst

--- lib/sales_analyst.rb
+++ lib/sales_analyst.rb
@@ -1,6 +1,5 @@
 require_relative 'sales_engine'
 require 'bigdecimal'
-require 'pry'
 
 class SalesAnalyst
   attr_reader :sales_engine
@@ -10,13 +9,10 @@ class SalesAnalyst
   end
 
   def total_number_of_merchants
-    # returns total count of all merchants in Merchant Repository
-    # use enum .inject instead of .count
     @sales_engine.merchants.all.count
   end
 
   def total_number_of_items
-    # returns total count of all items in Item Repository
     @sales_engine.items.all.count
   end
 
@@ -36,11 +32,10 @@ class SalesAnalyst
 
   def item_counts_for_each_merchants
     id_count_pairs = all_merchant_id_numbers
-    id_count_pairs.inject(Hash.new(0)) { |hash, item| hash[item] += 1; hash }.values
-    # id_count_pairs.inject(Hash.new(0)) { |hash, item| hash[item] += 1; hash }
+    id_count_pairs.inject(Hash.new(0)) { |hash, item| hash[item] += 1; hash }
   end
 
-  def combine_merchant_item_count
+  def combined_merchant_item_count
     item_counts = item_counts_for_each_merchants
     # avg = average_items_per_merchant
     avg = 2.9
@@ -54,8 +49,8 @@ class SalesAnalyst
     standard_deviation.round(1)
   end
 
-  def find_percentage_of_those_who_fall_one_std_dev_below
-    #find those below one std dev based on normal distribution graph, which is 15.8%
+  def get_number_of_merchants_one_stdv_away_from_mean
+    #changed method name to comform with the new spec, but method is the same
     total = total_number_of_merchants
     percentage = 0.158
     total * percentage
@@ -67,47 +62,89 @@ class SalesAnalyst
     items.sort_by { |key, value| value }
   end
 
-  def merchants_below_one_std_dev
+  def get_merchants_one_stdv_above_mean
     # this returns the merchant_ids with the item counts
     # takes sorted nested array of merchants, and extracts those below one std dev
     sorted = sort_merchants_based_on_the_number_of_listings
-    below_avg = find_percentage_of_those_who_fall_one_std_dev_below
-    # ROOT OF PROBLEM
+    above_avg = get_number_of_merchants_one_stdv_away_from_mean
+    sorted.last(above_avg).to_h.keys
     # binding.pry
-    sorted.first(below_avg)
   end
-
-  def merchants_with_low_item_count
-    # take array of merchants_below_one_std_dev
-    # extract merchant_ids
-    # map over standard merchants.all
-    # push the merchant.name of any matching merchants
-
-    merch_repo = @sales_engine.merchants
-    # binding.pry
-    merchants = merchants_below_one_std_dev
-    merchant_ids = merchants.to_h.keys
-
-    merchant_ids.map do |id|
-      merch_repo.find_by_id(id)
+# WORK ON THIS
+  def merchants_with_high_item_count
+    merchant_ids = get_merchants_one_stdv_above_mean
+    @sales_engine.merchants.all.select do |m|
+      merchant_ids.include?(m.id)
+      binding.pry
     end
   end
 
-
-  def average_item_price_for_merchant(merchant_id)
-    # merchant = @sales_engine.merchants.find_by_id(merchant_id)
-    merchant = @sales_engine.items.find_all_by_merchant_id(merchant_id)
+#find average prices of items based on merchant_ids
+  def get_hash_of_merchants_to_items
+    all_items = @sales_engine.items.all
+    merchant_to_items = {}
+    all_items.each do |item|
+      id = item.merchant_id
+      if !merchant_to_items.has_key?(id)
+        merchant_to_items[id] = [item]
+      else
+        merchant_to_items[id] << item
+      end
+    end
+    merchant_to_items
   end
 
-  # def find_one_standard_deviation_value
-  #   #(standard_deviation - mean) / 4
-  #   std_deviation = calc_items_per_merchant_standard_deviation
-  #   mean = average_items_per_merchant
-  #   # binding.pry
-  #   return (std_deviation - mean) / 4
-  #
-  #   #this will give one standard deviation to calculate
-  #   #who falls one sd down below.
-  # end
+  def average_item_price_for_merchants(merchant_id) #required method
+    merchant_to_items = get_hash_of_merchants_to_items
+    item_prices = merchant_to_items[merchant_id].map {|x| x.unit_price}
+    (item_prices.inject(:+)/item_prices.count)#.to_s
+  end
 
+  def average_price_per_merchant #required
+    all_items = @sales_engine.items.all
+    all_items.map {|item| item.unit_price}.inject(:+)/all_items.count
+    #result need to be .to_s??
+  end
+#finished relationship question 3 above, start question 4 below.
+  def sort_price_for_all_items
+    all_items = @sales_engine.items.all
+    all_items.map {|item| item.unit_price}.sort.reverse
+    #MAKE SURE THE SORT IS CORRECT
+  end
+
+  def get_number_of_items_that_fall_2_stdv_above
+    percentage_for_two_stdv_abv = 0.022
+    (total_number_of_items * 0.022).round(0)  #=>30
+  end
+
+  def items_with_2_std_dev_above_avg_price
+    sorted_prices = sort_price_for_all_items
+    top_priced = get_number_of_items_that_fall_2_stdv_above
+    sorted_prices.first(top_priced)
+  end  #THIS RETURNS 30 ITEMS
+
+  def golden_items
+    top_priced = items_with_2_std_dev_above_avg_price
+    @sales_engine.items.all.select do |item|
+      top_priced.include?(item.unit_price)
+    end.first(top_priced.count)
+  end  #THIS RETURNS 32 ITEMS INSTEAD OF 30. NEEDED TO ADD .FIRST()
+end
+ #finished iteration 1
+
+
+if __FILE__ == $0
+se = SalesEngine.from_csv({:merchants => './data/merchants.csv',
+                           :items => './data/items.csv'})
+
+sa = SalesAnalyst.new(se)
+# sa.get_hash_of_merchants_to_items
+# sa.average_item_price_for_merchants(12334275)
+# sa.get_total_price_for_all_items
+# sa.sort_price_for_all_items
+# sa.get_number_of_items_that_fall_2_stdv_above
+# puts sa.items_with_2_std_dev_above_avg_price.count
+# puts sa.golden_items.count
+# puts sa.get_merchants_one_stdv_above_mean
+sa.merchants_with_high_item_count
 end

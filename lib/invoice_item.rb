8c768a8200ade94cfbbcca3e016868cbd79a7b33
Matt Pindell <pindell.m@gmail.com>
2016-01-16 11:21:46 -0700

    class: Invoice Items w/ initialize method

---
+++ lib/invoice_item.rb
@@ -0,0 +1,25 @@
+class InvoiceItem
+  attr_accessor :id, :item_id, :invoice_id, :quantity, :unit_price, :created_at, :updated_at
+
+  def initialize(args_hash)
+    @id          = args_hash[:id].to_i
+    @item_id = args_hash[:item_id].to_i
+    @invoice_id = args_hash[:invoice_id].to_i
+    @quantity      = args_hash[:quantity]
+
+    if args_hash[:unit_price].is_a?(BigDecimal)
+      @unit_price = args_hash[:unit_price]
+    else
+      @unit_price  = BigDecimal.new(args_hash[:unit_price].insert(-3, "."),4)
+    end
+
+    @created_at  = Time.parse(args_hash[:created_at])
+    @updated_at  = Time.parse(args_hash[:updated_at])
+  end
+
+  def unit_price_to_dollars
+    # returns the price of the invoice item in dollars formatted as a Float
+
+  end
+
+end

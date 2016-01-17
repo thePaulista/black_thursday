7c4743a48afccc792fae32db9ba7567a5ef73455
Hedy <hedywoo@gmail.com>
2016-01-16 19:54:10 -0700

    corrected the failing test, that happened after previous merge

--- test/customer_repository_test.rb
+++ test/customer_repository_test.rb
@@ -112,12 +112,12 @@ class CustomerRepositoryTest < Minitest::Test
     assert_equal expected, submitted.count
   end
 
-  def test_find_by_all_last_names_returns_one_name
+  def test_find_by_all_last_names_returns_three_names
     last_name = "Osinski"
-    expected = last_name
+    expected = 3
     submitted = @cr.find_all_by_last_name(last_name)
 
-    assert_equal expected, submitted.last_name
+    assert_equal expected, submitted.count
   end
 
 end

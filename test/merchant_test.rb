f660afbee563c3fb4ff9c9e298db490ae031d2ef
Matt Pindell <pindell.m@gmail.com>
2016-01-16 18:32:29 -0700

    test: added testing for Merchant Class invoices method

--- test/merchant_test.rb
+++ test/merchant_test.rb
@@ -1,11 +1,25 @@
 require './test/test_helper'
 require './lib/merchant'
+require './lib/sales_engine'
 
 class MerchantTest < Minitest::Test
-  def test_inputs_are_name_and_id
-    m = Merchant.new({:id => 314151, :name => "Turing School"})
-    assert_equal 314151, m.id
-    assert_equal "Turing School", m.name
+
+  def setup
+    @merchant = Merchant.new({:id => 12334176, :name => "Turing School"})
+    @sales_engine = SalesEngine.from_csv({:invoices => './data/invoices.csv'})
   end
 
+  def test_inputs_are_name_and_id
+    assert_equal 12334176, @merchant.id
+    assert_equal "Turing School", @merchant.name
+  end
+
+  def test_invoices
+    merchant_id = @merchant.id
+    submitted = @sales_engine.invoices.find_all_by_merchant_id(merchant_id).to_a
+
+    assert_equal 21, submitted.count
+    assert_kind_of Array, submitted
+    assert_kind_of Invoice, submitted.first
+  end
 end

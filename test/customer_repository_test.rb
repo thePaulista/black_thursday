9cade05778353c858cc0f459892ba586660f6d11
Hedy <hedywoo@gmail.com>
2016-01-16 19:28:02 -0700

    customer_repo_test completed

--- test/customer_repository_test.rb
+++ test/customer_repository_test.rb
@@ -21,22 +21,95 @@ class CustomerRepositoryTest < Minitest::Test
   def test_all_method_gives_correct_count
 
     assert_equal 1000, @cr.all.count
+    refute_equal 0, @cr.all.count
   end
 
-  def test_find_by_customer_id_returns
+  def test_find_by_id_returns_id
     id = 2
     expected = id
     submitted = @cr.find_by_id(id)
-binding.pry
+
     assert_equal expected, submitted.id
   end
 
-  def test_find_by_all_first_names_returns_one_name
-    first_name = "Cecilia"
+  def test_find_by_customer_id_returns_nil
+    id = 1000000
+    expected = id
+    submitted = @cr.find_by_id(id)
+
+    assert_nil submitted
+  end
+
+  def test_find_by_all_first_names_returns_empty_when_not_found
+    first_name = "Bird"
     expected = first_name
     submitted = @cr.find_all_by_first_name(first_name)
 
-    assert_equal expected, submitted.first_name
+    assert_equal [], submitted
+  end
+
+  def test_find_by_all_first_name_given_single_character
+    first_name = "S"
+    expected = 221
+    submitted = @cr.find_all_by_first_name(first_name)
+
+    assert_equal expected, submitted.count
+  end
+
+  def test_find_by_all_first_name_given_three_character
+    first_name = "Sam"
+    expected = 4
+    submitted = @cr.find_all_by_first_name(first_name)
+
+    assert_equal expected, submitted.count
+  end
+
+  def test_find_by_all_first_name_given_case_insensitive_fragment
+    first_name = "joEY"
+    expected = 1
+    submitted = @cr.find_all_by_first_name(first_name)
+
+    assert_equal expected, submitted.count
+  end
+
+  def test_find_by_all_first_name_given_case_insensitive_fragment
+    first_name = "Cecilia"
+    expected = 1
+    submitted = @cr.find_all_by_first_name(first_name)
+
+    assert_equal expected, submitted.count
+  end
+
+  def test_find_by_all_last_name_returns_empty_array_with_bad_name
+    last_name = "123"
+    expected = []
+    submitted = @cr.find_all_by_last_name(last_name)
+
+    assert_equal expected, submitted
+  end
+
+  def test_find_by_all_last_name_given_a_single_fragment
+    last_name = "z"
+    expected = 61
+    submitted = @cr.find_all_by_last_name(last_name)
+
+    assert_equal expected, submitted.count
+  end
+
+  def test_find_by_all_last_name_given_three_fragments
+    last_name = "Oni"
+    expected = 2
+    submitted = @cr.find_all_by_last_name(last_name)
+
+    assert_equal expected, submitted.count
+  end
+
+  def test_find_by_all_last_name_given_case_insentive_name
+    last_name = "ConSiDiNe"
+    expected = 2
+    submitted = @cr.find_all_by_last_name(last_name)
+
+    assert_equal expected, submitted.count
   end
 
 end

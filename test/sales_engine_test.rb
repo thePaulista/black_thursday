7433f48c9e5caf07940a3d3f22e695b8621dac05
Matt Pindell <pindell.m@gmail.com>
2016-01-18 12:55:59 -0700

    Sales Engine Test - removed setup, added class variable

--- test/sales_engine_test.rb
+++ test/sales_engine_test.rb
@@ -2,26 +2,16 @@ require_relative 'test_helper'
 require_relative '../lib/sales_engine'
 
 class SalesEngineTest < Minitest::Test
-  # @@sales_engine = SalesEngine.from_csv({
-  #   :merchants     => './data/merchants.csv',
-  #   :items         => './data/items.csv',
-  #   :invoices      => './data/invoices.csv',
-  #   :invoice_items => './data/invoice_items.csv',
-  #   :transactions  => './data/transactions.csv',
-  #   :customers     => './data/customers.csv'})
-
-  def setup
-    @sales_engine = SalesEngine.from_csv({
-      :merchants     => './data/merchants.csv',
-      :items         => './data/items.csv',
-      :invoices      => './data/invoices.csv',
-      :invoice_items => './data/invoice_items.csv',
-      :transactions  => './data/transactions.csv',
-      :customers     => './data/customers.csv'})
-  end
+  @@sales_engine = SalesEngine.from_csv({
+    :merchants     => './data/merchants.csv',
+    :items         => './data/items.csv',
+    :invoices      => './data/invoices.csv',
+    :invoice_items => './data/invoice_items.csv',
+    :transactions  => './data/transactions.csv',
+    :customers     => './data/customers.csv'})
 
   def test_to_create_a_sales_engine_object_we_use_the_factory
-    assert_kind_of SalesEngine, @sales_engine
+    assert_kind_of SalesEngine, @@sales_engine
   end
 
   def test_sales_engine_can_receive_one_csv_file
@@ -45,7 +35,7 @@ class SalesEngineTest < Minitest::Test
   end
 
   def test_sales_engine_can_create_Merchant_Repos
-    merch_repo = @sales_engine.merchants
+    merch_repo = @@sales_engine.merchants
     expected = "#<Merchant:0"
     submitted = merch_repo.find_by_name("CJsDecor").to_s
 
@@ -54,7 +44,7 @@ class SalesEngineTest < Minitest::Test
   end
 
   def test_sales_engine_can_create_Items_Repos
-    item_repo = @sales_engine.items
+    item_repo = @@sales_engine.items
     expected = "#<Items:0"
     submitted = item_repo.find_by_name("510+ RealPush Icon Set").to_s
 
@@ -63,7 +53,7 @@ class SalesEngineTest < Minitest::Test
   end
 
   def test_sales_engine_can_create_Items_Repos
-    item_repo = @sales_engine.invoices
+    item_repo = @@sales_engine.invoices
     expected = "#<Invoice:0"
     submitted = item_repo.find_by_id(1).to_s
 
